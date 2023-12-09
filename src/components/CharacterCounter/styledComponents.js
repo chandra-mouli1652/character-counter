f59696bfb0d7e25537ee66e3a9eca51e855f6bf0
chandra-mouli1652 <chandubynapalepu21@gmail.com>
2023-12-09 06:33:41 +0000

    character counter solution

---
+++ src/components/CharacterCounter/styledComponents.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  display: flex;
+`
+
+export const LeftPanel = styled.div`
+  height: 100vh;
+  background-color: #ffc533;
+  width: 50%;
+`
+
+export const InfoCardContainer = styled.div`
+  background-color: #ffbf1f;
+`
+export const Info = styled.h1`
+  color: #334155;
+  font-size: 32px;
+  line-height: 50px;
+`
+
+export const RightPanel = styled.div`
+  background-color: #0f172a;
+  height: 100vh;
+  width: 50%;
+`
+
+export const CounterHeading = styled.h1`
+  color: #ffbf1f;
+  font-size: 32px;
+`
+
+export const AddInputContainer = styled.form`
+  display: flex;
+`
+export const Input = styled.input`
+  height: 35px;
+  width: 100%;
+  border-radius: 4px;
+  outline: none;
+  border: none;
+  padding-left: 15px;
+  color: #475569;
+`
+
+export const AddInputButton = styled.button`
+  background-color: #ffc533;
+  font-size: 14px;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  color: #334155;
+`
+
+export const UserInputsList = styled.ul`
+  margin-top: 50px;
+`
+
+export const EmptyImage = styled.img`
+  height: 400px;
+  width: 100%;
+`
